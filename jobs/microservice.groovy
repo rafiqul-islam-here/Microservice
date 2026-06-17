4d523257ee2df2d1b8c060890a8510faf8859c15
rafiqul-islam-here <Rafiqul.Islam@logicmatrix.tech>
2026-06-17 20:02:29 +0600

    Create microservice.groovy

---
+++ jobs/microservice.groovy
@@ -0,0 +1,67 @@
+def services = [
+    [
+        name: 'frontend',
+        branch: 'frontend',
+        image: 'frontend',
+        context: '.'
+    ],
+    [
+        name: 'cartservice',
+        branch: 'cartservice',
+        image: 'cartservice',
+        context: './src'
+    ]
+]
+
+services.each { svc ->
+
+    pipelineJob("${svc.name}-pipeline") {
+
+        definition {
+            cps {
+                script("""
+pipeline {
+    agent any
+
+    stages {
+
+        stage('Checkout') {
+            steps {
+                git branch: '${svc.branch}',
+                    url: 'https://github.com/rafiqhere/Microservice.git'
+            }
+        }
+
+        stage('Build') {
+            steps {
+                sh 'docker build -t rafiqhere/jenkins:${svc.image} ${svc.context}'
+            }
+        }
+
+        stage('Login') {
+            steps {
+                withCredentials([usernamePassword(
+                    credentialsId: 'docker-cred',
+                    usernameVariable: 'USER',
+                    passwordVariable: 'PASS'
+                )]) {
+                    sh 'echo "$PASS" | docker login -u "$USER" --password-stdin'
+                }
+            }
+        }
+
+        stage('Push') {
+            steps {
+                sh 'docker push rafiqhere/jenkins:${svc.image}'
+            }
+        }
+
+    }
+}
+                """.stripIndent())
+
+                sandbox()
+            }
+        }
+    }
+}
